2d433e5ec82b6490b8bd33571490a8b726592634
codeHasib <mohammadhasib9827@gmail.com>
2026-04-11 21:51:48 +0600

    checking the img path is correct or not in live link

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-import data from "@/public/data/personalInfo.json";
+import data1 from "@/data/personalInfo.json";
+import data2 from "@/data/portfolioData.json";
 
 export default async function Home() {
-  const res1 = await fetch("/data/personalInfo.json");
-  const data = await res1.json();
-  console.log(data)
   return <div>
-
+    {
+      data1.blogs.map((item, ind)=> <img src={item.image} key={ind}></img>)
+    }
+    {
+      data2.skills.frontend.map((item,ind)=> <img src={item.icon} key={ind}></img>)
+    }
   </div>;
 }
