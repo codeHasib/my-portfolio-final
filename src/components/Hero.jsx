255b767fcffdfd9217bc863e3acbb609613d7ba2
codeHasib <mohammadhasib9827@gmail.com>
2026-05-13 19:14:52 +0600

    fixed the height width of my image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
           damping: 20,
           delay: baseDelay + 0.1,
         }}
-        className="relative h-[350px] w-[350px] p-[4px] rounded-full overflow-hidden bg-gray-500 "
+        className="relative h-[300px] w-[300px] p-[4px] rounded-full overflow-hidden bg-gray-500 "
       >
         <div className="absolute inset-0 bg-[length:400%_400%] bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 animate-gradient rounded-full"></div>
 
@@ -37,7 +37,7 @@ const Hero = () => {
           <div className="rounded-full flex justify-center items-center">
             <Image
               fill
-              className="transition-all duration-300 hover:scale-100 rounded-full"
+              className="object-cover transition-all duration-300 hover:scale-100 rounded-full"
               src={"/myImg.png"}
               alt="Image of Mohammad Hasib"
             ></Image>
