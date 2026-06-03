0df039f353e2375a018a47c988011e4ebaf037b0
codeHasib <mohammadhasib9827@gmail.com>
2026-06-03 22:38:47 +0600

    updated resume

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -2,7 +2,7 @@ const ResumeButton = () => {
   return (
     <a
       href={"/Hasib_Premium_Resume.pdf"}
-      download={"/Mohammad_Hasib_Resume.pdf"}
+      download={"/Mohammad_Hasib_Junior_Full_Stack_Developer.pdf"}
       title="Download Resume"
       className="btn btn-accent"
     >
